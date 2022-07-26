23c909c63031b6e299a8fe279c36c1c3dc4abe42
KaLipSs0 <lucky_chrm@mail.ru>
2022-07-26 16:28:39 +0300

    SECRET_KEY is hidden

--- drf/settings.py
+++ drf/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w-1d@3o^p9u#4-4*qpys*gtk+tza3o-!0gylp130brx&nr^54k'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
